4af52345a13a45715fc1bbad6fa9e93f4e3f48da
Bobby Sanchez <sanchezbobby93@gmail.com>
2021-11-17 09:31:59 -0600

    Updated Models

--- tunr/models.py
+++ tunr/models.py
@@ -2,10 +2,9 @@ from django.db import models
 
 class Artist(models.Model):
     name = models.CharField(max_length=100)
-    nationality = models.CharField(max_length=100)
-    photo_url = models.TextField()
-    title = models.CharField(max_length=100)
-    album = models.CharField(max_length=100)
+    unit_name = models.CharField(max_length=100)
+    unit_upgrades = models.TextField()
+    unit_stats= models.CharField(max_length=100)
     def __str__(self):
         return self.name    
 
